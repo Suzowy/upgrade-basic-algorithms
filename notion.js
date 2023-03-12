f76dda1da778024d31c26bb8a3b777111d1bcda3
Suzowy <suzowy@gmail.com>
2023-03-12 18:14:36 +0100

    iteration#1

---
+++ notion.js
@@ -0,0 +1,21 @@
+"use strict";
+//interaccion 1: variables
+
+let myFavoriteHero = 'Hulk';
+let x = 50;
+let h = 5;
+let y = 10;
+let z = h + y;
+
+//interaccion2: variables avanzadas
+
+//const character ={ name: 'Jack Sparrow', age: 25};
+//let firstName = 'Jhon ';
+//let lastName = 'Snow ';
+//let age= 24;
+//console.log( 'soy ' + firstName + lastName + 'tengo ' + age + ' y me gustan los lobos.');
+
+
+
+
+
